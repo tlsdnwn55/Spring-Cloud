71e5e61858efcfd88b0b5fc2319d7d1fd25106dc
tlsdnwn55 <tlsdnwn55@naver.com>
2022-09-12 23:57:38 +0900

    kafka를 활용한 데이터 동기화 테스트 1

---
+++ catalogs-service/src/main/java/com/example/catalogsservice/messagequeue/KafkaConsumer.java
@@ -0,0 +1,46 @@
+package com.example.catalogsservice.messagequeue;
+
+import com.example.catalogsservice.jpa.CatalogEntity;
+import com.example.catalogsservice.jpa.CatalogRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@Slf4j
+public class KafkaConsumer {
+    CatalogRepository repository;
+
+    @Autowired
+    public KafkaConsumer(CatalogRepository repository) {
+        this.repository = repository;
+    }
+
+    @KafkaListener(topics = "example-catalog-topic")
+    public void updateQty(String kafkaMessage) {
+        log.info("Kafka Message: ->" + kafkaMessage);
+
+        /* 메세지 역직렬화 */
+        Map<Object, Object> map = new HashMap<>();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            map = mapper.readValue(kafkaMessage, new TypeReference<Map<Object, Object>>() {
+            }); // String 으로 들어온 kafkaMessage를 Json 으로 변환
+        } catch (JsonProcessingException ex) {
+            ex.printStackTrace();
+        }
+
+        CatalogEntity entity = repository.findByProductId((String) map.get("productId"));
+        if (entity != null) {
+            entity.setStock(entity.getStock() - (Integer) map.get("qty"));
+            repository.save(entity);
+        }
+    }
+}
--- order-service/src/main/java/com/example/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/example/orderservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.example.orderservice.controller;
 
 import com.example.orderservice.dto.OrderDto;
 import com.example.orderservice.jpa.OrderEntity;
+import com.example.orderservice.messagequeue.KafkaProducer;
 import com.example.orderservice.service.OrderService;
 import com.example.orderservice.vo.RequestOrder;
 import com.example.orderservice.vo.ResponseOrder;
@@ -22,11 +23,13 @@ public class OrderController {
 
     Environment env;
     OrderService orderService;
+    KafkaProducer kafkaProducer;
 
     @Autowired
-    public OrderController(Environment env, OrderService orderService) {
+    public OrderController(Environment env, OrderService orderService, KafkaProducer kafkaProducer) {
         this.env = env;
         this.orderService = orderService;
+        this.kafkaProducer = kafkaProducer;
     }
 
     @GetMapping("/health_check")
@@ -41,10 +44,15 @@ public class OrderController {
         ModelMapper mapper = new ModelMapper();
         mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 
+        /* jpa */
         OrderDto orderDto = mapper.map(orderDetails, OrderDto.class);
         orderDto.setUserId(userId);
         OrderDto createOrder = orderService.createOrder(orderDto);
+
         ResponseOrder responseOrder = mapper.map(createOrder,ResponseOrder.class);
+
+        /* send this order to the kafka */
+        kafkaProducer.send("example-category-topic", orderDto);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(responseOrder);
     }
